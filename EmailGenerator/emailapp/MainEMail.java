2004745384d6e1c0f955e4149b8de447522dfa77
CodeWithAkshi <sainipurti1@gmail.com>
2024-09-17 23:52:59 +0530

    Add files via upload

---
+++ EmailGenerator/emailapp/MainEMail.java
@@ -0,0 +1,18 @@
+package emailapp;
+
+import java.util.Scanner;
+
+public class MainEMail {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("                  Welcome to our Email Generation Portal                         ");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("Please enter your first Name:-");
+        String fname=sc.nextLine();
+        System.out.println("Please enter your last Name:-");
+        String lname=sc.nextLine();
+        System.out.println();
+        Email em1=new Email(fname,lname);
+    }
+}
